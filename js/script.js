7d60e3ece292d946d4782598466ca1cdefb5247d
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-02-24 10:57:59 -0600

    search function by zip

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ var apikey = "325ade0da4514bb29ff036144a8bc016"
 var bareLegislatorsUrl = "http://congress.api.sunlightfoundation.com/legislators/?apikey="
 var legislatorsUrl = bareLegislatorsUrl + apikey
 
-var bareZipUrl = "https://congress.api.sunlightfoundation.com/districts/locate/?zip="
+var bareZipUrl = "https://congress.api.sunlightfoundation.com/legislators/locate/?apikey=325ade0da4514bb29ff036144a8bc016&zip="
+
 
 // Search
 
@@ -13,17 +14,18 @@ var inputEl = document.querySelector('input[type="text"]')
 inputEl.value = "Enter zip here:"
 
 
-var zipToUrl = function(keyEvent) {	
-  	var inputEl = keyEvent.srcElement
-  		if (keyEvent.keyCode === 13) {
-      		zip = inputEl.value
-      		inputEl.value = ""
-      			console.log(zip)
-      		searchUrl = bareZipUrl + zip + "&apikey=" + apikey 
-      			console.log(searchUrl)   
-      	   	var zipPromise = $.getJSON(searchUrl)  
-      	   	console.log(zipPromise)	
+var doRequest = function(zipCode) {
+	var zipPromise = $.getJSON(bareZipUrl + zipCode)  
+	zipPromise.then(handleData)
+} 
 
+
+var zipToUrl = function(keyEvent) {	
+  	var inputEl = keyEvent.target
+  		if (keyEvent.keyCode === 13) {
+      		var zipCode = inputEl.value
+      		inputEl.value = ""
+      		doRequest(zipCode)
   }
 }
 
@@ -39,7 +41,7 @@ var legislatorToHTML = function(legislatorObj) {
 		newString += '<h3 class="legislatorName">' + legislatorObj.first_name + " " + legislatorObj.last_name + '</h3>' 
 		newString += '<h5 class="legislatorTitle">' + legislatorObj.title + " " + "(" + legislatorObj.party + "-" + legislatorObj.state_name + ")" + '</h5>'
 		newString += '<ul class="list">'  
-		newString += '<li><p>email:' + " " + legislatorObj.oc_email.toLowerCase() +'<p></li>'
+		newString += '<li><p>email:' + " " + legislatorObj.oc_email +'<p></li>'
 		newString += '<li><p>website:' + " " + '<a href="'+ legislatorObj.website +'" target="_blank">'+legislatorObj.website+'</a>' + '<p></li>' 
 		newString += '<li><p>facebook:' + " " + legislatorObj.facebook_id +'<p></li>'
 		newString += '<li><p>twitter:' + " " + legislatorObj.twitter_id +'<p></li>' 
